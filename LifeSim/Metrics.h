75ae4eff1a45a7d9a72c2eaf37ff3f724376015b
Julia Fantacone <JayFanta@users.noreply.github.com>
2015-10-06 19:11:44 -0400

    Update Metrics.h
    adding metrics and fixing meters

--- LifeSim/Metrics.h
+++ LifeSim/Metrics.h
@@ -12,8 +12,19 @@
 //Error tolerance for floating point calcualtions
 #define EPSILON 0.000001f
 
-//Difference threshold for distinguishing "species"
+//Difference threshold within seperating "species"
 #define DELTA 1000
+
+const float HOUR = 0.0416f;
+const float DAY = 1.0f;
+const float WEEK = 7.0f;
+const float MONTH = 31.0f;
+
+// mass metrics
+
+const float GRAM = 0.001f;
+const float KILOGRAM = 1f;
+
 
 //or
 //const float METER = 0.3f;

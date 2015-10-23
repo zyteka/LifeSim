b275d8fc7056a67d3c406b961fd03d66943de4e5
Behemyth <asher.norland@gmail.com>
2015-10-23 14:59:38 -0400

    updated terrain

--- LifeSim/Terrain.cpp
+++ LifeSim/Terrain.cpp
@@ -7,20 +7,13 @@ Terrain::Terrain(btDiscreteDynamicsWorld* worldN)
 	world = worldN;
 	position = glm::mat4();
 
-	Vertex fill = { { -1.0f*KILOMETER, 0.0f, -1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } };
-	GetVertices().push_back(fill);
-	Vertex fill1 = { { 1.0f*KILOMETER, 0.0f, 1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } };
-	GetVertices().push_back(fill1);
-	Vertex fill2 = { { -1.0f*KILOMETER, 0.0f, 1.0f*KILOMETER },{ 0.9f, 0.9f, 0.9f } };
-	GetVertices().push_back(fill2);
-	Vertex fill3 = { { 1.0f*KILOMETER, 0.0f, -1.0f*KILOMETER },{ 0.9f, 0.9f, 0.9f } };
-	GetVertices().push_back(fill3);
+	GetVertices().push_back({ { -1.0f*KILOMETER, 0.0f, -1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } });
+	GetVertices().push_back({ { 1.0f*KILOMETER, 0.0f, 1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } });
+	GetVertices().push_back({ { -1.0f*KILOMETER, 0.0f, 1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } });
+	GetVertices().push_back({ { 1.0f*KILOMETER, 0.0f, -1.0f*KILOMETER }, { 0.9f, 0.9f, 0.9f } });
 
-	Index fill4 = { glm::uvec3(0, 2, 1) };
-	GetIndices().push_back(fill4);
-	Index fill5 = { glm::uvec3(0, 1, 3) };
-	GetIndices().push_back(fill5);
-
+	GetIndices().push_back({ glm::uvec3(0, 2, 1) });
+	GetIndices().push_back({glm::uvec3(0, 1, 3)});
 
 
 	shape = new btStaticPlaneShape(btVector3(0, 1, 0), 0);

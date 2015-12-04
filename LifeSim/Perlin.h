d2dd71ce2a6d20e118065bbd23625911b57be63b
Sunny <sunny159753@gmail.com>
2015-12-04 16:40:31 -0500

    make pretty

--- LifeSim/Perlin.h
+++ LifeSim/Perlin.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 
 
-#define SAMPLE_SIZE 1024
-
+#define SAMPLE_SIZE 1024  
+ 
 class Perlin
 {
+
 public:
 
   Perlin(int octaves,float freq,float amp,int seed);
@@ -23,12 +24,14 @@ public:
   };
 
 private:
+
   void init_perlin(int n,float p);
   float perlin_noise_2D(float vec[2]);
 
   float noise1(float arg);
   float noise2(float vec[2]);
   float noise3(float vec[3]);
+
   void normalize2(float v[2]);
   void normalize3(float v[3]);
   void init(void);
@@ -39,9 +42,11 @@ private:
   int   mSeed;
 
   int p[SAMPLE_SIZE + SAMPLE_SIZE + 2];
+
   float g3[SAMPLE_SIZE + SAMPLE_SIZE + 2][3];
   float g2[SAMPLE_SIZE + SAMPLE_SIZE + 2][2];
   float g1[SAMPLE_SIZE + SAMPLE_SIZE + 2];
+
   bool  mStart;
 
 };
